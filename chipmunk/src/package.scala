7c9f43c3ce0650ba7662b5904e0f2fdfc4d04f4e
zhutmost <zhutmost@outlook.com>
2023-06-02 16:15:22 +0800

    feat: add misc methods to Bits and Data

--- chipmunk/src/package.scala
+++ chipmunk/src/package.scala
@@ -2,6 +2,10 @@ import chisel3._
 import chisel3.util._
 
 package object chipmunk {
+
+  /** A Bundle with no elements. */
+  final class EmptyBundle extends Bundle
+
   implicit class AddMethodsToBits[T <: Bits](c: T) {
 
     /** returns the most significant n bits. */
@@ -10,7 +14,13 @@ package object chipmunk {
     /** returns the least significant n bits. */
     def lsb(n: Int = 1): UInt = c(n - 1, 0)
 
-    /** sets all the bits to the specified Bool input. */
+    /** returns the most significant bit as Bool. */
+    def msb: Bool = c.msb().asBool
+
+    /** returns the least significant bit as Bool. */
+    def lsb: Bool = c.lsb().asBool
+
+    /** sets all the bits to the specified Bool literal. */
     def setAllTo(b: Bool): T = {
       c := Fill(c.getWidth, b).asTypeOf(c)
       c
@@ -24,5 +34,24 @@ package object chipmunk {
 
     /** sets all the bits to False. */
     def clearAll(): T = setAllTo(false)
+
+    /** returns whether the number is one-hot encoded. */
+    def isOneHot: Bool = {
+      val parity = Wire(Vec(c.getWidth, Bool()))
+      parity(0) := c(0)
+      for (i <- 1 until c.getWidth) {
+        parity(i) := parity(i - 1) ^ c(i)
+      }
+      val ret = parity(c.getWidth - 1) && (parity.asUInt | (~c).asUInt).andR
+      ret
+    }
+  }
+
+  implicit class AddMethodsToData[T <: Data](c: T) {
+
+    /** Mark this signal as an optimization barrier to Chisel and FIRRTL. Same as `chisel3.dontTouch(x)`. */
+    def dontTouch: T = {
+      chisel3.dontTouch(c)
+    }
   }
 }
