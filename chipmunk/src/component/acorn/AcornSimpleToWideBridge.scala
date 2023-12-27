ccf848fcaebae32ba1ae58a8bb881933ea003e62
zhutmost <zhutmost@outlook.com>
2023-12-27 22:32:51 +0800

    feat: add several acorn bridges

---
+++ chipmunk/src/component/acorn/AcornSimpleToWideBridge.scala
@@ -0,0 +1,57 @@
+package chipmunk
+package component.acorn
+import chipmunk.stream._
+import chisel3._
+import chisel3.util._
+
+class AcornSimpleToWideBridge(
+  dataWidth: Int = 32,
+  addrWidth: Int = 32,
+  statusWidth: Int = 2,
+  maskUnit: Int = 0,
+  outstanding: Int = 16
+) extends Module {
+  val io = IO(new Bundle {
+    val sAcornS = Slave(new AcornSimpleIO(addrWidth, dataWidth, statusWidth, maskUnit))
+    val mAcornW = Master(new AcornWideIO(addrWidth, dataWidth, statusWidth, maskUnit))
+  })
+
+  // ---------------------------------------------------------------------------
+  // Command channel
+
+  val cmdDemux   = StreamDemux(in = io.sAcornS.cmd, select = io.sAcornS.cmd.bits.read.asUInt, num = 2)
+  val cmdDemuxWr = cmdDemux(0)
+  val cmdDemuxRd = cmdDemux(1)
+
+  io.mAcornW.wr.cmd handshakeFrom cmdDemuxWr
+  io.mAcornW.wr.cmd.bits.addr  := cmdDemuxWr.bits.addr
+  io.mAcornW.wr.cmd.bits.wdata := cmdDemuxWr.bits.wdata
+  if (io.mAcornW.hasMask) {
+    io.mAcornW.wr.cmd.bits.wmask.get := cmdDemuxWr.bits.wmask.get
+  }
+
+  io.mAcornW.rd.cmd handshakeFrom cmdDemuxRd
+  io.mAcornW.rd.cmd.bits.addr := cmdDemuxRd.bits.addr
+
+  // ---------------------------------------------------------------------------
+  // Select generation
+
+  val selectPush: StreamIO[UInt] = Wire(Stream(io.sAcornS.cmd.bits.read.asUInt))
+  selectPush.bits  := io.sAcornS.cmd.bits.read
+  selectPush.valid := io.sAcornS.cmd.fire
+
+  val selectPop: StreamIO[UInt] = Queue(enq = selectPush, entries = outstanding, pipe = true, flow = true)
+
+  // ---------------------------------------------------------------------------
+  // Response channel
+
+  val respMuxWr = Wire(Stream(io.sAcornS.resp))
+  val respMuxRd = Wire(Stream(io.sAcornS.resp))
+  val respMux   = StreamMux(select = selectPop, ins = VecInit(respMuxWr, respMuxRd))
+  respMuxWr.bits.rdata  := 0.U
+  respMuxWr.bits.status := io.mAcornW.wr.resp.bits.status
+  respMuxRd.bits.rdata  := io.mAcornW.rd.resp.bits.rdata
+  respMuxRd.bits.status := io.mAcornW.rd.resp.bits.status
+
+  io.sAcornS.resp << respMux
+}
---
+++ chipmunk/src/component/acorn/AcornWideToSimpleBridge.scala
@@ -0,0 +1,68 @@
+package chipmunk
+package component.acorn
+
+import stream._
+
+import chisel3.util._
+import chisel3._
+
+class AcornWideToSimpleBridge(
+  dataWidth: Int = 32,
+  addrWidth: Int = 32,
+  statusWidth: Int = 2,
+  maskUnit: Int = 0,
+  outstanding: Int = 16
+) extends Module {
+  val io = IO(new Bundle {
+    val sAcornW = Slave(new AcornWideIO(addrWidth, dataWidth, statusWidth, maskUnit))
+    val mAcornS = Master(new AcornSimpleIO(addrWidth, dataWidth, statusWidth, maskUnit))
+  })
+
+  // ---------------------------------------------------------------------------
+  // Command channel
+
+  val cmdArbiterWr = Wire(Stream(io.mAcornS.cmd))
+  val cmdArbiterRd = Wire(Stream(io.mAcornS.cmd))
+  val cmdArbiter   = StreamArbiter.roundRobin(ins = Seq(cmdArbiterWr, cmdArbiterRd))
+
+  cmdArbiterWr handshakeFrom io.sAcornW.wr.cmd
+  cmdArbiterWr.bits.addr  := io.sAcornW.wr.cmd.bits.addr
+  cmdArbiterWr.bits.read  := false.B
+  cmdArbiterWr.bits.wdata := io.sAcornW.wr.cmd.bits.wdata
+  if (io.sAcornW.hasMask) {
+    cmdArbiterWr.bits.wmask.get := io.sAcornW.wr.cmd.bits.wmask.get
+  }
+
+  cmdArbiterRd handshakeFrom io.sAcornW.rd.cmd
+  cmdArbiterRd.bits.addr  := io.sAcornW.rd.cmd.bits.addr
+  cmdArbiterRd.bits.read  := true.B
+  cmdArbiterWr.bits.wdata := 0.U
+  if (io.sAcornW.hasMask) {
+    cmdArbiterWr.bits.wmask.get := Fill(io.sAcornW.maskWidth, true.B)
+  }
+
+  io.mAcornS.cmd << cmdArbiter
+
+  // ---------------------------------------------------------------------------
+  // Select generation
+
+  val selectPush: StreamIO[UInt] = Wire(Stream(io.mAcornS.cmd.bits.read.asUInt))
+  selectPush.bits  := io.mAcornS.cmd.bits.read
+  selectPush.valid := io.mAcornS.cmd.fire
+
+  val selectPop: StreamIO[UInt] = Queue(enq = selectPush, entries = outstanding, pipe = true, flow = true)
+
+  // ---------------------------------------------------------------------------
+  // Response channel
+
+  val respDemux   = StreamDemux(in = io.mAcornS.resp, select = selectPop, num = 2)
+  val respDemuxWr = respDemux(0)
+  val respDemuxRd = respDemux(1)
+
+  io.sAcornW.wr.resp handshakeFrom respDemuxWr
+  io.sAcornW.rd.resp.bits.status := respDemuxRd.bits.status
+  io.sAcornW.rd.resp.bits.rdata  := respDemuxRd.bits.rdata
+
+  io.sAcornW.rd.resp handshakeFrom respDemuxRd
+  io.sAcornW.wr.resp.bits.status := respDemuxWr.bits.status
+}
